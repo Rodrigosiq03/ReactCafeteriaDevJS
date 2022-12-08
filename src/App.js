ce3fc2de61efa207385f32aaffcae56f2df41b04
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2022-12-08 18:42:48 -0300

    login feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
   return (
     <div>
       <Signup/>
+      <Login/>
     </div>
   );
 }
